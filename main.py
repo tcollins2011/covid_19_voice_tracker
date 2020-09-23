3fa9ddab3b12d747435f3eb3a0b68475a7fc50f9
Tylre Collins <tyler@nhlp.com>
2020-09-23 13:21:29 -0600

    added speech to text and regex

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import pyttsx3
+import re
+import speech_recognition as sr
+
 
 API_KEY = 'tDLjW0sFmB5X'
 PROJECT_TOKEN = 'tz1ObDDHpiZm'
@@ -41,6 +45,77 @@ class Data:
         
         return "0"
 
-data = Data(API_KEY, PROJECT_TOKEN)
+    def get_list_of_countires(self):
+        countries = []
+        for country in self.data['Country']:
+            countries.append(country['name'].lower())
+
+        return countries
+        
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ''
+
+        try: 
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print ("Exception:", str(e))
+
+    return said.lower()
 
 
+
+
+def main():
+    print("Started Program")
+    data = Data(API_KEY, PROJECT_TOKEN) 
+    END_PHRASE = 'stop'
+    country_list = data.get_list_of_countires()
+        
+    TOTAL_PATTERNS = { 
+        re.compile("[\w\s]+ total [\w\s]+ cases"): data.get_total_cases,
+        re.compile("[\w\s]+ total cases"): data.get_total_cases,
+        re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+        re.compile("[\w\s]+ total deaths"): data.get_total_deaths,   
+    }
+
+    COUNTRY_PATTERNS = {
+        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+        re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
+    }
+    while True:
+        print("Listening...")
+
+        text = get_audio()
+        print(text)
+        result = None
+
+        for pattern, func in COUNTRY_PATTERNS.items():
+            if pattern.match(text):
+                words = set(text.split(" "))
+                for country in country_list:
+                    if country in words:
+                        result = func(country)
+                        break
+
+        for pattern, func in TOTAL_PATTERNS.items():
+            if pattern.match(text):
+                result = func()
+                break
+
+        if result:
+            speak(result)
+    
+        if text.find(END_PHRASE) != -1:
+            print('Ended Program')
+            break
+
+main()
